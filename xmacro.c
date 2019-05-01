b72dcca2cb88f781caa722aef0ab3c21519617ca
Adam Thornton <athornton@lsst.org>
2019-05-01 16:07:56 -0700

    Prettified debugging output.

--- xmacro.c
+++ xmacro.c
@@ -3,38 +3,65 @@
 
 zword cw(void) {
     extern zbyte *pcp;
+    extern zbyte *zmp;
+    long pc;
     zword retval;
-    
-    printf("cw entry: PCP is 0x%x\n", pcp);
+
+    pc = (long) (pcp - zmp);
+    fprintf(stderr, \
+            "DEBUG: cw   entry: PC = 0x%lx; PCP = 0x%p; ZMP = 0x%p\n", \
+            pc, pcp, zmp);
 
     retval = (zword) (256 * pcp[0] + pcp[1] );
     pcp += 2;
-    
-    /* pcpt = ((unsigned int ) pcp) & 0xffff;
-    pcp = (zbyte *) pcpt; */
-    printf("cw exit: return value is 0x%x, PCP is 0x%x\n", retval, pcp);
+    pc = (long) (pcp - zmp);
+
+    fprintf(stderr, \
+            "DEBUG: cw   sp   : PC = 0x%lx; PCP = 0x%p; ZMP = 0x%p\n", \
+            pc, pcp, zmp);
+
+    /* Check for overflow */
+    if (pc > 0xffff) {
+        fprintf(stderr, "DEBUG: Code_word wrap detected: PC = 0x%lx\n", pc);
+        /* Don't fix up...
+        offset &= 0xffff;
+        pcp = (zbyte *)((long) zmp + offset);
+        */
+    }
+
+    fprintf(stderr, \
+            "DEBUG: cw   exit : PC = 0x%lx; ret = 0x%x\n", \
+            pc,retval);
     return retval;
 }
 
 long g_pc(void) {
-    long v;
+    long pc;
     extern zbyte *pcp, *zmp;
-    printf("g_pc entry: PCP is 0x%x, ZMP is 0x%x\n", pcp, zmp);
-    v = (long) (pcp - zmp);
-    /*printf("g_pc mid: V is 0x%x, PCP is 0x%x, ZMP is 0x%x\n", v, pcp, zmp);
-      v &= 0xffff; */
-    printf("g_pc exit: V is 0x%x, PCP is 0x%x, ZMP is 0x%x\n", v, pcp, zmp);    
-    return v;
+    fprintf(stderr, \
+            "DEBUG: g_pc entry:              PCP = 0x%p; ZMP = 0x%p\n", \
+            pcp, zmp);
+    pc = (long) (pcp - zmp);
+    if ( pc > 0xffff) {
+        fprintf(stderr, "DEBUG: PC wrap detected (get): 0x%lx\n", pc);
+    }
+    fprintf(stderr, \
+            "DEBUG: g_pc exit : PC = 0x%lx, PCP = 0x%p, ZMP = 0x%p\n", \
+            pc, pcp, zmp);
+    return pc;
 }
 
-void s_pc(long v) {
+void s_pc(long pc) {
     extern zbyte *pcp, *zmp;
-    long q;
-    printf("s_pc entry: V is 0x%x, PCP is 0x%x, ZMP is 0x%x\n", v, pcp, zmp);
-    q = (long ) ( zmp + v);
-    /* q &= 0xffff; */
-    pcp = (zbyte *) q;
-    printf("s_pc exit: V is 0x%x, PCP is 0x%x, ZMP is 0x%x\n", v, pcp, zmp);
+    fprintf(stderr, \
+            "DEBUG: s_pc entry: PC = 0x%lx, PCP = 0x%p, ZMP = 0x%p\n", \
+            pc, pcp, zmp);
+    if ( pc > 0xffff) {
+        fprintf(stderr, "DEBUG: PC wrap detected (set): 0x%lx\n", pc);
+    }
+    pcp = (zbyte *) (pc + (long) zmp);
+    fprintf(stderr, \
+            "DEBUG: s_pc exit : PC = 0x%lx, PCP = 0x%p, ZMP = 0x%p\n", \
+            pc, pcp, zmp);
     return;
 }
-
